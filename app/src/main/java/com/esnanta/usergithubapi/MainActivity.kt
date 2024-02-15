2bc41a173535cfca426ef63fab340a50ff9d850e
Nanta Es <ombakrinai@gmail.com>
2024-02-15 21:22:12 +0700

    update app bar

--- app/src/main/java/com/esnanta/usergithubapi/MainActivity.kt
+++ app/src/main/java/com/esnanta/usergithubapi/MainActivity.kt
@@ -2,10 +2,34 @@ package com.esnanta.usergithubapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.esnanta.usergithubapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        binding.topAppBar.setOnMenuItemClickListener {menuItem ->
+//            when (menuItem.itemId){
+//                R.id.menu1 -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.fragment_container, MenuFragment())
+//                        .addToBackStack(null)
+//                        .commit()
+//                    true
+//                }
+//                R.id.menu2 -> {
+//                    val intent = Intent(this, MenuActivity::class.java)
+//                    startActivity(intent)
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
     }
+
+
 }
