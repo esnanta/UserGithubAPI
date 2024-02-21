6e6578659cab2f682dd8dfce6ee75bf13fb1000a
Nanta Es <ombakrinai@gmail.com>
2024-02-21 20:42:49 +0700

    refactor file

--- app/src/main/java/com/esnanta/usergithubapi/ui/SearchFragment.kt
+++ app/src/main/java/com/esnanta/usergithubapi/ui/SearchFragment.kt
@@ -1,21 +1,22 @@
-package com.esnanta.usergithubapi
+package com.esnanta.usergithubapi.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.fragment.app.Fragment
+import com.esnanta.usergithubapi.R
 
 /**
  * A simple [Fragment] subclass.
  * Use the [SearchFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 class SearchFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
