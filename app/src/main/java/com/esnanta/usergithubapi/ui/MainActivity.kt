e0016a755877543c4cee37d2fd10a38a5a36b3a2
Nanta Es <ombakrinai@gmail.com>
2024-03-04 16:14:54 +0700

    need to redisign layout

--- app/src/main/java/com/esnanta/usergithubapi/ui/MainActivity.kt
+++ app/src/main/java/com/esnanta/usergithubapi/ui/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.esnanta.usergithubapi.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.esnanta.usergithubapi.data.response.UserItem
 import com.esnanta.usergithubapi.databinding.ActivityMainBinding
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewUserModel : ViewModel by viewModels()
 
     companion object {
-        private const val TAG = "MainActivity"
-        private const val USER_GITHUB_ID = "xxxxx"
+        private val TAG = MainActivity::class.java.simpleName
+        private const val USERNAME_GITHUB = "sidiqpermana"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,13 +31,80 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val userItem = DummyData.getDummyData(this)
-        val userAdapter = UserAdapter(userItem)
+        val layoutManager = LinearLayoutManager(this)
+        binding.userRecyclerView.setLayoutManager(layoutManager)
+        val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
+        binding.userRecyclerView.addItemDecoration(itemDecoration)
 
-        val recyclerViewNews = binding.userRecyclerView
-        recyclerViewNews.adapter = userAdapter
-        recyclerViewNews.layoutManager = LinearLayoutManager(this)
-        recyclerViewNews.setHasFixedSize(true)
+        getUsers()
+        //val userItem = DummyData.getDummyData(this)
+        //val userAdapter = UserAdapter(userItem)
+
+//        val recyclerViewNews = binding.userRecyclerView
+//        recyclerViewNews.adapter = userAdapter
+//        recyclerViewNews.layoutManager = LinearLayoutManager(this)
+//        recyclerViewNews.setHasFixedSize(true)
 
     }
+
+    private fun getUsers(){
+        binding.progressBar.visibility = View.VISIBLE
+
+        val client = AsyncHttpClient()
+        val url = "https://api.github.com/search/users?q=$USERNAME_GITHUB"
+
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?
+            ) {
+                val userItem = arrayListOf<UserItem>()
+                val result = responseBody?.let { String(it) }
+                Log.d(TAG, result.toString())
+                try {
+                    val responseObject = JSONObject(result)
+
+                    val jsonArray = JSONArray(result)
+
+                    for (i in 0 until jsonArray.length()) {
+                        val jsonObject = jsonArray.getJSONObject(i)
+                        val username = jsonObject.getString("login")
+                        val id = jsonObject.getString("id")
+                        val userItemDetail = UserItem(
+                            Integer.parseInt(id.toString()),
+                            username
+                        )
+                        userItem.add(userItemDetail)
+                    }
+
+                    val userAdapter = UserAdapter(userItem)
+                    binding.userRecyclerView.adapter = userAdapter
+
+                } catch (e: Exception) {
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?,
+                error: Throwable?
+            ) {
+                binding.progressBar.visibility = View.INVISIBLE
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error?.message}"
+                }
+
+                Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+
 }
--- app/src/main/java/com/esnanta/usergithubapi/ui/UserViewModel.kt
+++ app/src/main/java/com/esnanta/usergithubapi/ui/UserViewModel.kt
@@ -1,12 +1,23 @@
 package com.esnanta.usergithubapi.ui
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.esnanta.usergithubapi.data.response.UserItem
 
 class UserViewModel: ViewModel() {
-//    private val users: MutableLiveData<List<User>> by lazy {
-//        MutableLiveData<List<User>>().also {
-//            loadUsers()
-//        }
-//    }
+    private val users: MutableLiveData<List<UserItem>> by lazy {
+        MutableLiveData<List<UserItem>>().also {
+            loadUsers()
+        }
+    }
+
+    fun getUsers(): LiveData<List<UserItem>> {
+        return users
+    }
+
+    private fun loadUsers() {
+        // Do an asynchronous operation to fetch users.
+    }
 }
