0e70cd82b07bf1a2d24f7dcde5a6202b0d6fc990
Nanta Es <ombakrinai@gmail.com>
2024-03-09 10:02:37 +0700

    add property debug for api config

--- app/src/main/java/com/esnanta/usergithubapi/data/retrofit/ApiConfig.kt
+++ app/src/main/java/com/esnanta/usergithubapi/data/retrofit/ApiConfig.kt
@@ -1,5 +1,6 @@
 package com.esnanta.usergithubapi.data.retrofit
 
+import com.loopj.android.http.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -7,16 +8,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiConfig {
     companion object{
+        private const val DEBUG = true
         fun getApiService(): ApiService {
 
-//            val loggingInterceptor = if(BuildConfig.DEBUG) {
-//                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-//            } else {
-//                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
-//            }
-
-            val loggingInterceptor =
+            val loggingInterceptor = if(DEBUG) {
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
 
             val client = OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
