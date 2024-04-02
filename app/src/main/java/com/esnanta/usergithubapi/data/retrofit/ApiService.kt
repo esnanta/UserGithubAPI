37d32af97a8b82c90a297fce9dac0157d6a2547a
Nanta Es <ombakrinai@gmail.com>
2024-04-02 23:12:34 +0700

    add build config variable

--- app/src/main/java/com/esnanta/usergithubapi/data/retrofit/ApiService.kt
+++ app/src/main/java/com/esnanta/usergithubapi/data/retrofit/ApiService.kt
@@ -1,5 +1,6 @@
 package com.esnanta.usergithubapi.data.retrofit
 
+import com.esnanta.usergithubapi.BuildConfig
 import com.esnanta.usergithubapi.data.response.FollowerResponseItem
 import com.esnanta.usergithubapi.data.response.FollowingResponseItem
 import com.esnanta.usergithubapi.data.response.SearchResponse
@@ -13,18 +14,18 @@ import retrofit2.http.Query
 interface ApiService {
 
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.TOKEN_GITHUB}")
     fun getSearch(@Query("q") query: String): Call<SearchResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.TOKEN_GITHUB}")
     fun getDetail(@Path("username") username: String): Call<UserResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.TOKEN_GITHUB}")
     fun getListFollower(@Path("username") username: String): Call<List<FollowerResponseItem>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.TOKEN_GITHUB}")
     fun getListFollowing(@Path("username") username: String): Call<List<FollowingResponseItem>>
 }
